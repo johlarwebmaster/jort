dacde4110904fb6bfaa0845edd19823e05edad5e
Raul Smith <raul.n.smith@gmail.com>
2021-09-29 18:11:22 -0600

    Fixing Header Issues

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,15 +5,12 @@ import logo from '../logo.png';
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="md" bg="light" variant="light" fixed="top" className="shadow p-3 mb-5 bg-body rounded">
+    <Navbar expand="md" bg="light" variant="light" fixed="top" className="shadow p-3 mb-5 bg-body rounded">
         <Navbar.Brand href="/">
             <img src={logo} height="60" width="auto" alt="Junk or Treasures" />&nbsp;&nbsp;
             <span className="text-primary title-text">Junk or Treasures</span>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav" className="d-flex flex-row-reverse">
-            <GoogleAuth />
-        </Navbar.Collapse>
+        <GoogleAuth />
     </Navbar>
   );
 }
