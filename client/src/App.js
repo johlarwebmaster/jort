7ff08a479d736765facdad7213b686fd71c76a54
john <you@example.com>
2021-11-10 20:05:19 -0600

    Refactor index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from "react";
+//Pages
 import Home from './pages/Home';
 import ItemList from './pages/ItemList';
 import SellerPage from './pages/SellerPage';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+
+//Firebase
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import {ReactReduxFirebaseProvider} from 'react-redux-firebase'
+import { getFirebase } from 'react-redux-firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import {firebaseConfig} from './firebaseConfig.js'
+
+//Stripe
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
 import { Router, Route } from 'react-router-dom';
@@ -20,7 +38,34 @@ function App() {
     setDevice(mobile);
   }, []);
 
+
+
+  const rfConfig = {
+  userProfile: 'items'
+  }
+
+  firebase.initializeApp(firebaseConfig)
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk.withExtraArgument(getFirebase))));
+
+  const stripePromise = loadStripe('pk_test_A7jK4iCYHL045qgjjfzAfPxu');
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: '{{CLIENT_SECRET}}',
+  };
+
+
+
+  
   return (
+    <Provider store={store}>
+    <ReactReduxFirebaseProvider
+    firebase={firebase}
+    config={rfConfig}
+    dispatch={store.dispatch}
+    >
+    <Elements stripe={stripePromise} options={options}>
     <Router history={history}>
       <Route path="/" exact component={Home} />
       <Header />
@@ -29,7 +74,11 @@ function App() {
       <Route path="/profile" component={Profile} />
       {!device ? <Footer /> : <MobileNav />}
     </Router>
+    </Elements>
+    </ReactReduxFirebaseProvider>
+    </Provider>
   );
 }
 
 export default App;
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './reducers';
-import {ReactReduxFirebaseProvider} from 'react-redux-firebase'
-import { getFirebase } from 'react-redux-firebase'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
-import {firebaseConfig} from './firebaseConfig'
-
-const rfConfig = {
-  userProfile: 'items'
-}
-
-firebase.initializeApp(firebaseConfig)
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk.withExtraArgument(getFirebase))));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ReactReduxFirebaseProvider
-      firebase={firebase}
-      config={rfConfig}
-      dispatch={store.dispatch}
-    >
-      <App />
-    </ReactReduxFirebaseProvider>
-  </Provider>,
+      <App />,
   document.getElementById('root')
 );
